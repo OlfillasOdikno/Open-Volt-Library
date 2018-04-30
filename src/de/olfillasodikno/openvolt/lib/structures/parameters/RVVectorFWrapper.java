fedc6cfe417d161dd632f604246bb25e4d38f31b
Felipe <felipe@custodio-romero.de>
2018-04-30 18:19:25 +0200

    [*] Fix last commit

--- src/de/olfillasodikno/openvolt/lib/structures/parameters/RVVectorFWrapper.java
+++ src/de/olfillasodikno/openvolt/lib/structures/parameters/RVVectorFWrapper.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 
 import de.olfillasodikno.openvolt.lib.structures.RVVectorF;
 
-
-public class RVVectorFWrapper extends ParamWrapper<RVVectorF>{
+public class RVVectorFWrapper extends ParamWrapper<RVVectorF> {
 
 	public RVVectorFWrapper() {
 		super(RVVectorF.class);
@@ -13,15 +12,37 @@ public class RVVectorFWrapper extends ParamWrapper<RVVectorF>{
 
 	@Override
 	public boolean wrap(ArrayList<Object> data, RVVectorF in) {
-		if(data.size()!=3) {
+		if (data.size() != 3) {
 			return false;
 		}
 		Object x = data.get(0);
 		Object y = data.get(1);
 		Object z = data.get(2);
-		if(!(x instanceof Float && y instanceof Float && z instanceof Float) && !(x instanceof Integer && y instanceof Integer && z instanceof Integer)) {
+
+		if (x instanceof Integer) {
+			x = ((Integer) x).floatValue();
+		}
+		if (y instanceof Integer) {
+			y = ((Integer) y).floatValue();
+		}
+		if (z instanceof Integer) {
+			z = ((Integer) z).floatValue();
+		}
+
+		if (x instanceof Float) {
+			x = ((Float) x).floatValue();
+		}
+		if (y instanceof Float) {
+			y = ((Float) y).floatValue();
+		}
+		if (z instanceof Float) {
+			z = ((Float) z).floatValue();
+		}
+		if (!(x.getClass().equals(float.class) && y.getClass().equals(float.class)
+				&& z.getClass().equals(float.class))) {
 			return false;
 		}
+
 		in.setX((float) x);
 		in.setY((float) y);
 		in.setZ((float) z);
